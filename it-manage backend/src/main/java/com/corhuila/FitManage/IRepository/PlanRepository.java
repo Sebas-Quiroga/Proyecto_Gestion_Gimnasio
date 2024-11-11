154b44a527127561a3dcc5ad2e56f11b02af61df
Sebas-Quiroga <125420726+Sebas-Quiroga@users.noreply.github.com>
2024-11-11 14:26:12 -0500

    Revert "backen y frontend"
    This reverts commit 3c48e48c71679c894a96a4bacb12107a1d581295.

--- it-manage backend/src/main/java/com/corhuila/FitManage/IRepository/PlanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.corhuila.FitManage.IRepository;
-
-import com.corhuila.FitManage.Entity.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-
-}
